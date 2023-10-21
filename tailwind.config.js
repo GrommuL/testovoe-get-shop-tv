471f77ba891be398a20fbb9df3c1613b44a579ac
Grommul <miketsvg@gmail.com>
2023-10-21 13:00:14 +0600

    update: добавил пути к картинкам для backgroundImage

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ export default {
         black: '#000000',
         blue: '#86D3F4'
       }
+    },
+    backgroundImage: {
+      qr: 'url("/src/assets/images/qr.png")',
+      main: 'url("/src/assets/images/bg-main.jpg")'
     }
   },
   plugins: []
