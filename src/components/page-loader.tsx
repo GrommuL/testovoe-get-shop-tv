4cfff3521f7702c9da63c681d04b93cf1abae9b1
Grommul <miketsvg@gmail.com>
2023-10-21 11:24:14 +0600

    feat: добавил спиннер для страниц

---
+++ src/components/page-loader.tsx
@@ -0,0 +1,7 @@
+export const PageLoader = () => {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+      <span className='w-[48px] h-[48px] border-[5px] border-blue border-solid border-b-transparent rounded-[50%] inline-block box-border animate-spin'></span>
+    </div>
+  )
+}
