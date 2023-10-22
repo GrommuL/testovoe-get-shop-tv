31cc34b7e651ebdcb8b41983e091b3254dbbe8c6
Grommul <miketsvg@gmail.com>
2023-10-22 14:06:21 +0600

    feat: добавил компонент close-icon

--- src/components/icons/close-icon.tsx
+++ src/components/icons/close-icon.tsx
@@ -1,7 +1,7 @@
 export const CloseIcon = () => {
   return (
     <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'>
-      <path d='M1 1L15 15M15 1L1 15' stroke-width='1.5' stroke-linecap='round' />
+      <path d='M1 1L15 15M15 1L1 15' strokeWidth='1.5' strokeLinecap='round' />
     </svg>
   )
 }
