65d2ad16e19ebd604622499e58f701c50af7baf9
Grommul <miketsvg@gmail.com>
2023-10-23 14:28:09 +0600

    fix: добавил setTelValue

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -4,14 +4,16 @@ import InputMask from 'react-input-mask'
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   mask: string | (string | RegExp)[]
+  setTelValue: React.Dispatch<React.SetStateAction<string>>
 }
 
-export const Input: FC<Props> = ({ className, mask, ...otherProps }) => {
+export const Input: FC<Props> = ({ className, mask, setTelValue, ...otherProps }) => {
   return (
     <InputMask
       mask={mask}
+      onChange={(e) => setTelValue((prev) => prev + e.target.value)}
       className={clsx(
-        'w-full text-[32px] font-bold uppercase outline-none border-none bg-transparent placeholder:text-black max-w-[280px]',
+        'w-full text-[32px] font-bold uppercase outline-none border-none bg-transparent placeholder:text-black max-w-[265px]',
         className
       )}
       {...otherProps}
